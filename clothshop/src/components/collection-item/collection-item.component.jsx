b4652783e4745e437005830892348d4d4526d86a
surajk202 <kawalesuraj76@gmail.com>
2019-10-24 01:33:55 +0530

    created cart utils and updated shop data

--- clothshop/src/components/collection-item/collection-item.component.jsx
+++ clothshop/src/components/collection-item/collection-item.component.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import './collection-item.scss'
+import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.action';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
+const CollectionItem = ({ item,addItem }) => {
+    
+    const { id, name ,price,imageUrl} = item;
+    return(
     <div className='collection-item'>
       <div
         className='image'
@@ -13,7 +19,18 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
+      <CustomButton onClick={()=>addItem(item)}
+      inverted
+      >
+        ADD TO CART
+      </CustomButton>
     </div>
-  );
+    )
   
-  export default CollectionItem;
+};
+
+  const mapDispatchToProps = dispatch => ({
+    addItem:item => dispatch(addItem(item))
+  })
+  
+  export default connect(null,mapDispatchToProps)(CollectionItem);
--- clothshop/src/redux/cart/cart.action.js
+++ clothshop/src/redux/cart/cart.action.js
@@ -2,3 +2,7 @@ export const toogleCartHidden =()=> ({
   type:'TOGGLE_CART_HIDDEN'    
 });
 
+export const addItem = item =>({
+    type:'ADD_ITEM',
+    payload:item
+})
