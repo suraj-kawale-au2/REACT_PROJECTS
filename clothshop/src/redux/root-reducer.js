afe08d31650d25ea51b925e15dbda6456d816d0d
surajk202 <kawalesuraj76@gmail.com>
2019-11-13 11:23:32 +0530

    added directory state to redux

--- clothshop/src/redux/root-reducer.js
+++ clothshop/src/redux/root-reducer.js
@@ -1,8 +1,20 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userReducer from './user/user.reducer';
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
 
-export default combineReducers({
+const persisitConfig = {
+    key : 'root',
+    storage,
+    whitelist : ['cart']
+}
+
+const rootReducer = combineReducers({
     user:userReducer,
-    cart:cartReducer
-});
+    cart:cartReducer,
+    directory:directoryReducer
+})
+
+export default persistReducer(persisitConfig,rootReducer)
